9b2407590d3d2efcbc49096f073523057fc0a78c
Aidan Willett <Gamer9310@gmail.com>
2020-01-23 18:16:20 -0800

    added error where name didnt show

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.use(routes);
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// require("./routes/api-routes.js")(app);
-
-// db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
-// });
+
